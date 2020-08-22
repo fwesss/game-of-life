d8ab99b5b25e4f5f48c27a781a2f63a51f50514d
Westley Feller <feller.westley@gmail.com>
2020-08-22 14:46:53 -0700

    Removed unnecessary boilerplate test

--- test/testUtils.ts
+++ test/testUtils.ts
@@ -1,20 +1,9 @@
 import { render } from '@testing-library/react'
-// import { ThemeProvider } from "my-ui-lib"
-// import { TranslationProvider } from "my-i18n-lib"
-// import defaultStrings from "i18n/en-x-default"
+import { FunctionComponent } from 'react'
 
-const Providers = ({ children }) => {
-  return children
-  // return (
-  //   <ThemeProvider theme="light">
-  //     <TranslationProvider messages={defaultStrings}>
-  //       {children}
-  //     </TranslationProvider>
-  //   </ThemeProvider>
-  // )
-}
+const Providers: FunctionComponent<any> = ({ children }) => children
 
-const customRender = (ui, options = {}) =>
+const customRender = (ui: JSX.Element, options = {}) =>
   render(ui, { wrapper: Providers, ...options })
 
 // re-export everything
